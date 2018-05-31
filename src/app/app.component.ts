106f789e086b3589fbcaae868ec40e54f93199ff
Jorge Resendiz Sanchez <egroj2_4@hotmail.com>
2018-05-30 21:04:43 -0500

    faltan 2 secciones de diseño y se esta responsivando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+//**aqui metere los archivos de los plugs o */
+
+
+title = 'app';
 }
